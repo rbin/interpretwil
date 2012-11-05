6e878fecbf1118f287c942c75981256160407f88
Robin J <robin@sosour.com>
2012-11-05 10:29:05 +0000

    Update readthis.rb

--- readthis.rb
+++ readthis.rb
@@ -1,7 +1,9 @@
 readthis.rb
 
+=begin
 Changed from the Twilio-ruby gem to SG's Twilio-rb gem.  Let's see how it goes!
 
 .. Changed back to Twilio-ruby gem...
 
 Didn't read ANY documentation, will do tomorrow, and will re-use the Twilio-rb gem...
+=end
